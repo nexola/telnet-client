cb1790f8912b3f8d511432137022f8f3b9ab72b9
nexola <vitormatheusfv@gmail.com>
2023-12-20 15:59:36 -0300

    Seções de formulários adicionadas

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
 // const Telnet = require("telnet-client");
 // const connection = new Telnet();
 
-const btnForm = document.querySelector("#btn__form");
-const inputHostname = document.querySelector("#hostname");
-const inputVlan = document.querySelector("#vlan");
-const inputInterface = document.querySelector("#interface");
-const inputSwitchport = document.querySelector("#switchport");
-const inputAccess = document.querySelector("#access");
-const inputIpgateway = document.querySelector("#ip-gateway");
-
-const inputs = [
-  inputHostname,
-  inputVlan,
-  inputInterface,
-  inputSwitchport,
-  inputAccess,
-  inputIpgateway,
-];
+const btnForm = document.querySelector(".btn__form");
+const inputs = document.querySelectorAll(".valor__input");
 
 const credenciais = {};
 
 btnForm.addEventListener("click", function (e) {
   e.preventDefault();
 
-  credenciais.hostname = inputHostname.value;
-  credenciais.vlan = inputVlan.value;
-  credenciais.interface = inputInterface.value;
-  credenciais.switchport = inputSwitchport.value;
-  credenciais.access = inputAccess.value;
-  credenciais.ipGateway = inputIpgateway.value;
-
-  inputs.forEach((input) => {
-    input.value = null;
+  inputs.forEach((i) => {
+    credenciais[`${i.id}`] = i.value;
+    i.value = null;
   });
 });
+
+console.log(credenciais);
