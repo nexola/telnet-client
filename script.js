f7a9835dd8a0d2b3151d536c065bf7adb32c53a6
nexola <vitormatheusfv@gmail.com>
2023-12-21 12:23:53 -0300

    Adicionando multiplos itens ao vlan

--- script.js
+++ script.js
@@ -1,18 +1,99 @@
-// const Telnet = require("telnet-client");
-// const connection = new Telnet();
+// Seletores
+const btnForm = document.querySelectorAll(".btn__form");
+const sections = document.querySelectorAll("section");
+const menuSection = document.querySelector("#menu--section");
+const allInputs = document.querySelectorAll(".valor__input");
+const btnVlan = document.querySelector("#add__vlan");
+const inputVlan = document.querySelector("#vlan");
+const itemVlan = document.querySelector(".item__vlan");
+const itemForm = document.querySelector(".item__form--vlan");
 
-const btnForm = document.querySelector(".btn__form");
-const inputs = document.querySelectorAll(".valor__input");
+// Funções auxiliares
+const zerarInputs = function () {
+  allInputs.forEach((i) => (i.value = null));
+};
 
-const credenciais = {};
+// Coletando valores dos inputs
+// Seção 1 / 2 / 3
+let entries = {};
+btnForm.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
 
-btnForm.addEventListener("click", function (e) {
+    if (entries.length > 0) {
+      entries = {};
+    }
+
+    const clicked = e.target;
+    const inputs = document.querySelectorAll(
+      `.section__${clicked.dataset.form} .valor__input`
+    );
+
+    inputs.forEach((i) => {
+      entries[`${i.id}`] = i.value;
+    });
+    zerarInputs();
+  })
+);
+
+console.log(entries);
+
+// Mudando aba
+const mudarAba = function (slide) {
+  sections.forEach((s) => s.classList.remove("section__ativo"));
+  slide.classList.add("section__ativo");
+  zerarInputs();
+};
+
+menuSection.addEventListener("click", function (e) {
   e.preventDefault();
 
-  inputs.forEach((i) => {
-    credenciais[`${i.id}`] = i.value;
-    i.value = null;
+  const clicked = e.target;
+  const abaAtiva = document.querySelector(`.section__${clicked.dataset.slide}`);
+  const abas = menuSection.querySelectorAll("button");
+
+  abas.forEach((a) => {
+    if (clicked !== a) {
+      return;
+    }
+    mudarAba(abaAtiva);
   });
 });
 
-console.log(credenciais);
+// Adicionando múltiplos vlan
+let itensVlan = new Set();
+
+const addItemVlan = function (valor) {
+  const txt = `<div class="item__vlan" data-valor="${valor}">
+  <h3>${valor}</h3>
+  <a href="" class="remove__vlan">x</a>
+  </div>`;
+  itemVlan.insertAdjacentHTML("beforebegin", txt);
+};
+
+btnVlan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (inputVlan.value && !itensVlan.has(inputVlan.value)) {
+    itensVlan.add(inputVlan.value);
+    console.log(itensVlan);
+    addItemVlan(inputVlan.value);
+    inputVlan.value = null;
+  }
+});
+
+itemForm.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let clicked = e.target;
+
+  if (!clicked.classList.contains("remove__vlan")) {
+    return;
+  }
+
+  let divClicked = clicked.closest("div");
+
+  itensVlan.delete(divClicked.dataset.valor);
+
+  divClicked.parentNode.removeChild(divClicked);
+});
