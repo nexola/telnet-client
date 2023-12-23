1ab10a9101014db76526991aa96dafdc39910882
nexola <vitormatheusfv@gmail.com>
2023-12-23 14:43:47 -0300

    Estilização das abas e inputs

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const btnVlan = document.querySelector("#add__vlan");
 const inputVlan = document.querySelector("#vlan");
 const itemVlan = document.querySelector(".item__vlan");
 const itemForm = document.querySelector(".item__form--vlan");
+const btnMenu = document.querySelectorAll("#menu--section button");
+
+const init = function () {
+  coletarValores(btnForm);
+  mudarAba(sections, menuSection);
+  addVlan(itemVlan, inputVlan);
+};
 
 // Funções auxiliares
 const zerarInputs = function () {
@@ -15,85 +22,99 @@ const zerarInputs = function () {
 
 // Coletando valores dos inputs
 // Seção 1 / 2 / 3
-let entries = {};
-btnForm.forEach((btn) =>
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
+const coletarValores = function (listaBtn) {
+  let entries = {};
+  listaBtn.forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
 
-    if (entries.length > 0) {
-      entries = {};
-    }
+      if (entries.length > 0) {
+        entries = {};
+      }
 
-    const clicked = e.target;
-    const inputs = document.querySelectorAll(
-      `.section__${clicked.dataset.form} .valor__input`
-    );
+      const clicked = e.target;
+      const inputs = document.querySelectorAll(
+        `.section__${clicked.dataset.form} .valor__input`
+      );
 
-    inputs.forEach((i) => {
-      entries[`${i.id}`] = i.value;
-    });
-    zerarInputs();
-  })
-);
-
-console.log(entries);
-
-// Mudando aba
-const mudarAba = function (slide) {
-  sections.forEach((s) => s.classList.remove("section__ativo"));
-  slide.classList.add("section__ativo");
-  zerarInputs();
+      inputs.forEach((i) => {
+        entries[`${i.id}`] = i.value;
+      });
+      zerarInputs();
+    })
+  );
 };
 
-menuSection.addEventListener("click", function (e) {
-  e.preventDefault();
+// Mudando aba
+const mudarAba = function (secoes, menu) {
+  const alternarAba = function (slide) {
+    secoes.forEach((s) => s.classList.remove("section__ativo"));
+    btnMenu.forEach((b) => b.classList.remove("btn__ativo"));
+    const btnAtivo = document.querySelector(
+      `#menu--section button[data-slide="${slide.dataset.slide}"]`
+    );
+    btnAtivo.classList.add("btn__ativo");
+    slide.classList.add("section__ativo");
+    zerarInputs();
+  };
 
-  const clicked = e.target;
-  const abaAtiva = document.querySelector(`.section__${clicked.dataset.slide}`);
-  const abas = menuSection.querySelectorAll("button");
+  menu.addEventListener("click", function (e) {
+    e.preventDefault();
 
-  abas.forEach((a) => {
-    if (clicked !== a) {
-      return;
-    }
-    mudarAba(abaAtiva);
+    const clicked = e.target;
+    const abaAtiva = document.querySelector(
+      `.section__${clicked.dataset.slide}`
+    );
+    const abas = menu.querySelectorAll("button");
+
+    abas.forEach((a) => {
+      if (clicked !== a) {
+        return;
+      }
+      alternarAba(abaAtiva);
+    });
   });
-});
+};
 
 // Adicionando múltiplos vlan
-let itensVlan = new Set();
+const addVlan = function (divInput, input) {
+  let itens = new Set();
 
-const addItemVlan = function (valor) {
-  const txt = `<div class="item__vlan" data-valor="${valor}">
+  const addHTMLVlan = function (valor) {
+    const txt = `<div class="item__vlan" data-valor="${valor}">
   <h3>${valor}</h3>
   <a href="" class="remove__vlan">x</a>
   </div>`;
-  itemVlan.insertAdjacentHTML("beforebegin", txt);
+    divInput.insertAdjacentHTML("beforebegin", txt);
+  };
+
+  btnVlan.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (input.value && !itens.has(input.value)) {
+      itens.add(input.value);
+      console.log(itens);
+      addHTMLVlan(input.value);
+      input.value = null;
+    }
+  });
+
+  itemForm.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let clicked = e.target;
+
+    if (!clicked.classList.contains("remove__vlan")) {
+      return;
+    }
+
+    let divClicked = clicked.closest("div");
+
+    itens.delete(divClicked.dataset.valor);
+
+    divClicked.parentNode.removeChild(divClicked);
+    return itens;
+  });
 };
 
-btnVlan.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  if (inputVlan.value && !itensVlan.has(inputVlan.value)) {
-    itensVlan.add(inputVlan.value);
-    console.log(itensVlan);
-    addItemVlan(inputVlan.value);
-    inputVlan.value = null;
-  }
-});
-
-itemForm.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  let clicked = e.target;
-
-  if (!clicked.classList.contains("remove__vlan")) {
-    return;
-  }
-
-  let divClicked = clicked.closest("div");
-
-  itensVlan.delete(divClicked.dataset.valor);
-
-  divClicked.parentNode.removeChild(divClicked);
-});
+init();
